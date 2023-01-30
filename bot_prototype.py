bd85fc612472539464bf9f98e75019b45a8f64c8
William <rvw112@gmail.com>
2023-01-30 20:12:38 +1300

    Class change and external api test
    Changing the class from discord.Client to its sub-class commands.Bot and testing external api requests

--- bot_prototype.py
+++ bot_prototype.py
@@ -1,21 +1,53 @@
+# Imports
+import os 
 import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
-# TOKEN = place token here...
+import aiohttp
+# import requests
+
+load_dotenv()
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents = intents)
+### ---------- discord.Client ---------- ### 
+# Represents a client connection that connects to Discord. 
+# This class is used to interact with the Discord WebSocket and API.
 
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
+# client = discord.Client(intents = intents)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+### ---------- commands.Bot ---------- ### 
+# Represents a Discord bot. 
+# This is a sub class of 'discord.Client' and as a result 
+# anything you can do with 'discord.Client' you can do with this bot.
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+bot = commands.Bot(command_prefix = "$", intents = intents)
 
-client.run(TOKEN)
+# Greeting test
+@bot.command(name = "hello")
+async def greeting(ctx):
+    username = str(ctx.author).split('#')[0]
+    await ctx.channel.send(f"Greetings {username}!")
+
+# Print test
+@bot.command()
+async def print(ctx, *args):
+    response = ""
+    for arg in args:
+        response = response + " " + arg
+    await ctx.channel.send(response)
+
+# API request test
+@bot.command(name = "dog")
+async def dog_image(ctx):
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get("https://dog.ceo/api/breeds/image/random") as r:
+            data = await r.json()
+            embed = discord.Embed(title = "Woof")
+            embed.set_image(url = data['message'])
+            await ctx.send(embed = embed)
+
+# Run the bot
+bot.run(os.environ.get('TOKEN'))
